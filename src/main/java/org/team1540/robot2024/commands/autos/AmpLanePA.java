c11289c58161228f6d2955b7c3520d91950f108d
belowAverageCoder5188 <146988904+belowAverageCoder5188@users.noreply.github.com>
2024-02-21 08:39:49 -0800

    Fix: Made AmpLanePAB and AmpLanePA shoot P note.

--- src/main/java/org/team1540/robot2024/commands/autos/AmpLanePA.java
+++ src/main/java/org/team1540/robot2024/commands/autos/AmpLanePA.java
@@ -8,22 +8,18 @@ import org.team1540.robot2024.subsystems.shooter.Shooter;
 import org.team1540.robot2024.util.auto.AutoCommand;
 import org.team1540.robot2024.util.auto.PathHelper;
 
-public class AmpLanePAB extends AutoCommand {
-    public AmpLanePAB (Shooter shooter, Indexer indexer, Drivetrain drivetrain) {
-        super("AmpLanePAB");
+import java.util.function.BooleanSupplier;
+
+public class AmpLanePA extends AutoCommand {
+    public AmpLanePA (Drivetrain drivetrain, Shooter shooter, Indexer indexer) {
+        super("AmpLanePA");
         addPath(
-                PathHelper.fromChoreoPath("AmpLanePAB.1"),
-                PathHelper.fromChoreoPath("AmpLanePAB.2"),
-                PathHelper.fromChoreoPath("AmpLanePAB.3")
+                PathHelper.fromChoreoPath("AmpLanePA.1")
         );
         addCommands(
                 getPath(0).getCommand(drivetrain),
                 new ShootSequence(shooter, indexer),
                 new IntakeCommand(indexer, () -> false, 100), //TODO: Tune this
-                new ShootSequence(shooter, indexer),
-                getPath(1).getCommand(drivetrain),
-                new ShootSequence(shooter, indexer),
-                getPath(2).getCommand(drivetrain),
                 new ShootSequence(shooter, indexer)
         );
     }
