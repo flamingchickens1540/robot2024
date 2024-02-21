35fefe69a8257f40748078448d15d648b264c3c1
belowAverageCoder5188 <146988904+belowAverageCoder5188@users.noreply.github.com>
2024-02-20 17:25:01 -0800

    Feat: DoNothing auto

---
+++ src/main/java/org/team1540/robot2024/commands/autos/DoNothing.java
@@ -0,0 +1,9 @@
+package org.team1540.robot2024.commands.autos;
+
+import org.team1540.robot2024.util.auto.AutoCommand;
+
+public class DoNothing extends AutoCommand {
+    public DoNothing () {
+        super("DoNothing");
+    }
+}
