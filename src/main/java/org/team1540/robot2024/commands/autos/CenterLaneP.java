0c877b657fd12322e90f26edaa24e730675a1fdd
belowAverageCoder5188 <146988904+belowAverageCoder5188@users.noreply.github.com>
2024-02-21 17:00:16 -0800

    Fix: made CenterLaneP not break

--- src/main/java/org/team1540/robot2024/commands/autos/CenterLaneP.java
+++ src/main/java/org/team1540/robot2024/commands/autos/CenterLaneP.java
@@ -1,19 +1,20 @@
 package org.team1540.robot2024.commands.autos;
 
-import org.team1540.robot2024.commands.indexer.IntakeCommand;
 import org.team1540.robot2024.commands.shooter.ShootSequence;
+import org.team1540.robot2024.subsystems.drive.Drivetrain;
 import org.team1540.robot2024.subsystems.indexer.Indexer;
 import org.team1540.robot2024.subsystems.shooter.Shooter;
 import org.team1540.robot2024.util.auto.AutoCommand;
 import org.team1540.robot2024.util.auto.PathHelper;
 
 public class CenterLaneP extends AutoCommand {
-    public CenterLaneP (Shooter shooter, Indexer indexer) {
+    public CenterLaneP (Shooter shooter, Indexer indexer, Drivetrain drivetrain) {
         super("CenterLaneP");
         addPath(
-                PathHelper.fromChoreoPath("CenterLaneP")
+                PathHelper.fromChoreoPath("CenterLaneP.1")
         );
         addCommands(
+                getPath(0).getCommand(drivetrain),
                 new ShootSequence(shooter, indexer)
         );
     }
