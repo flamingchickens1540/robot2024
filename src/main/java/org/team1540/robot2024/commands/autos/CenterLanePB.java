0b88f40114853e65e54463e971568831869add4f
belowAverageCoder5188 <146988904+belowAverageCoder5188@users.noreply.github.com>
2024-02-21 17:00:55 -0800

    Feat: created CenterLanePB auto

---
+++ src/main/java/org/team1540/robot2024/commands/autos/CenterLanePB.java
@@ -0,0 +1,26 @@
+package org.team1540.robot2024.commands.autos;
+
+import org.team1540.robot2024.commands.indexer.IntakeCommand;
+import org.team1540.robot2024.commands.shooter.ShootSequence;
+import org.team1540.robot2024.subsystems.drive.Drivetrain;
+import org.team1540.robot2024.subsystems.indexer.Indexer;
+import org.team1540.robot2024.subsystems.shooter.Shooter;
+import org.team1540.robot2024.util.auto.AutoCommand;
+import org.team1540.robot2024.util.auto.PathHelper;
+
+import java.util.function.BooleanSupplier;
+
+public class CenterLanePB extends AutoCommand {
+    public CenterLanePB (Drivetrain drivetrain, Shooter shooter, Indexer indexer) {
+        super("CenterLanePB");
+        addPath(
+                PathHelper.fromChoreoPath("CenterLanePB.1")
+        );
+        addCommands(
+                getPath(0).getCommand(drivetrain),
+                new ShootSequence(shooter, indexer),
+                new IntakeCommand(indexer, () -> false, 100), //TODO: Tune this
+                new ShootSequence(shooter, indexer)
+        );
+    }
+}
