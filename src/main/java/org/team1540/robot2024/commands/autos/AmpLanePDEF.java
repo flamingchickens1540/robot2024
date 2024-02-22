2d3dd8d8450da7ca1faa14b75c51683321d5a056
belowAverageCoder5188 <146988904+belowAverageCoder5188@users.noreply.github.com>
2024-02-21 17:26:19 -0800

    Feat: created AmpLanePDEF auto

---
+++ src/main/java/org/team1540/robot2024/commands/autos/AmpLanePDEF.java
@@ -0,0 +1,40 @@
+package org.team1540.robot2024.commands.autos;
+
+import org.team1540.robot2024.commands.indexer.IntakeCommand;
+import org.team1540.robot2024.commands.shooter.ShootSequence;
+import org.team1540.robot2024.subsystems.drive.Drivetrain;
+import org.team1540.robot2024.subsystems.indexer.Indexer;
+import org.team1540.robot2024.subsystems.shooter.Shooter;
+import org.team1540.robot2024.util.auto.AutoCommand;
+import org.team1540.robot2024.util.auto.PathHelper;
+
+public class AmpLanePDEF extends AutoCommand {
+    public AmpLanePDEF (Drivetrain drivetrain, Shooter shooter, Indexer indexer) {
+        super("AmpLanePDEF");
+        addPath(
+                PathHelper.fromChoreoPath("AmpLanePDEF.1"),
+                PathHelper.fromChoreoPath("AmpLanePDEF.2"),
+                PathHelper.fromChoreoPath("AmpLanePDEF.3"),
+                PathHelper.fromChoreoPath("AmpLanePDEF.4"),
+                PathHelper.fromChoreoPath("AmpLanePDEF.5"),
+                PathHelper.fromChoreoPath("AmpLanePDEF.6"),
+                PathHelper.fromChoreoPath("AmpLanePDEF.7")
+        );
+        addCommands(
+                getPath(0).getCommand(drivetrain),
+                new ShootSequence(shooter, indexer),
+                getPath(1).getCommand(drivetrain),
+                new IntakeCommand(indexer, () -> false, 100),
+                getPath(2).getCommand(drivetrain),
+                new ShootSequence(shooter, indexer),
+                getPath(3).getCommand(drivetrain),
+                new IntakeCommand(indexer, () -> false, 100),
+                getPath(4).getCommand(drivetrain),
+                new ShootSequence(shooter, indexer),
+                getPath(5).getCommand(drivetrain),
+                new IntakeCommand(indexer, () -> false, 100),
+                getPath(6).getCommand(drivetrain),
+                new ShootSequence(shooter, indexer)
+        );
+    }
+}
