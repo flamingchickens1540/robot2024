d5eccae0f71044375920ceebf71abce9b8a63e0f
belowAverageCoder5188 <146988904+belowAverageCoder5188@users.noreply.github.com>
2024-02-22 16:12:55 -0800

    Feat: created CenterLanePBC auto

---
+++ src/main/java/org/team1540/robot2024/commands/autos/CenterLanePBC.java
@@ -0,0 +1,30 @@
+package org.team1540.robot2024.commands.autos;
+
+import org.team1540.robot2024.commands.indexer.IntakeCommand;
+import org.team1540.robot2024.commands.shooter.ShootSequence;
+import org.team1540.robot2024.subsystems.drive.Drivetrain;
+import org.team1540.robot2024.subsystems.indexer.Indexer;
+import org.team1540.robot2024.subsystems.shooter.Shooter;
+import org.team1540.robot2024.util.auto.AutoCommand;
+import org.team1540.robot2024.util.auto.PathHelper;
+
+public class CenterLanePBC extends AutoCommand {
+    public CenterLanePBC (Drivetrain drivetrain, Shooter shooter, Indexer indexer) {
+        super("CenterLanePBC");
+        addPath(
+                PathHelper.fromChoreoPath("CenterLanePBC.1"),
+                PathHelper.fromChoreoPath("CenterLanePBC.2"),
+                PathHelper.fromChoreoPath("CenterLanePBC.3")
+        );
+        addCommands(
+                getPath(0).getCommand(drivetrain),
+                new ShootSequence(shooter, indexer),
+                new IntakeCommand(indexer, () -> false, 100), //TODO: tune this
+                new ShootSequence(shooter, indexer),
+                getPath(1).getCommand(drivetrain),
+                getPath(2).getCommand(drivetrain),
+                new IntakeCommand(indexer, () -> false, 100),
+                new ShootSequence(shooter, indexer)
+        );
+    }
+}
