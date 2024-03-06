a24740f527c531aaaeb0f7a3000b93fd5ff66fc6
belowAverageCoder5188 <146988904+belowAverageCoder5188@users.noreply.github.com>
2024-03-05 18:14:17 -0800

    Feat: created SourceLanePHGFED auto

---
+++ src/main/java/org/team1540/robot2024/commands/autos/SourceLanePHGFED.java
@@ -0,0 +1,50 @@
+package org.team1540.robot2024.commands.autos;
+
+import org.team1540.robot2024.commands.indexer.IntakeCommand;
+import org.team1540.robot2024.commands.shooter.ShootSequence;
+import org.team1540.robot2024.subsystems.drive.Drivetrain;
+import org.team1540.robot2024.subsystems.indexer.Indexer;
+import org.team1540.robot2024.subsystems.shooter.Shooter;
+import org.team1540.robot2024.util.auto.AutoCommand;
+import org.team1540.robot2024.util.auto.PathHelper;
+
+public class SourceLanePHGFED extends AutoCommand {
+    public SourceLanePHGFED (Drivetrain drivetrain, Indexer indexer, Shooter shooter) {
+        super("SourceLanePHGFED");
+        addPath(
+                PathHelper.fromChoreoPath("SourceLanePHGFED.1", true, true),
+                PathHelper.fromChoreoPath("SourceLanePHGFED.2"),
+                PathHelper.fromChoreoPath("SourceLanePHGFED.3"),
+                PathHelper.fromChoreoPath("SourceLanePHGFED.4"),
+                PathHelper.fromChoreoPath("SourceLanePHGFED.5"),
+                PathHelper.fromChoreoPath("SourceLanePHGFED.6"),
+                PathHelper.fromChoreoPath("SourceLanePHGFED.7"),
+                PathHelper.fromChoreoPath("SourceLanePHGFED.8"),
+                PathHelper.fromChoreoPath("SourceLanePHGFED.9"),
+                PathHelper.fromChoreoPath("SourceLanePHGFED.10")
+        );
+        addCommands(
+                new ShootSequence(shooter, indexer),
+                getPath(0).getCommand(drivetrain),
+                new IntakeCommand(indexer, () -> false, 1),
+                getPath(1).getCommand(drivetrain),
+                new ShootSequence(shooter, indexer),
+                getPath(0).getCommand(drivetrain),
+                new IntakeCommand(indexer, () -> false, 1),
+                getPath(1).getCommand(drivetrain),
+                new ShootSequence(shooter, indexer),
+                getPath(0).getCommand(drivetrain),
+                new IntakeCommand(indexer, () -> false, 1),
+                getPath(1).getCommand(drivetrain),
+                new ShootSequence(shooter, indexer),
+                getPath(0).getCommand(drivetrain),
+                new IntakeCommand(indexer, () -> false, 1),
+                getPath(1).getCommand(drivetrain),
+                new ShootSequence(shooter, indexer),
+                getPath(0).getCommand(drivetrain),
+                new IntakeCommand(indexer, () -> false, 1),
+                getPath(1).getCommand(drivetrain),
+                new ShootSequence(shooter, indexer)
+        );
+    }
+}
