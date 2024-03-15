83fea185e62439ec82a21935cfef4e61f0f4a46e
Simon Weil <113215817+WeilSimon@users.noreply.github.com>
2024-03-15 10:39:39 -0700

    feat: good shooter tuning command

--- src/main/java/org/team1540/robot2024/commands/shooter/TuneShooterCommand.java
+++ src/main/java/org/team1540/robot2024/commands/shooter/TuneShooterCommand.java
@@ -2,35 +2,37 @@ package org.team1540.robot2024.commands.shooter;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import org.littletonrobotics.junction.networktables.LoggedDashboardNumber;
+import org.team1540.robot2024.commands.indexer.IntakeAndFeed;
 import org.team1540.robot2024.subsystems.indexer.Indexer;
 import org.team1540.robot2024.subsystems.shooter.Shooter;
+import org.team1540.robot2024.util.shooter.ShooterSetpoint;
 
-public class TuneShooterCommand extends Command {
-    private final LoggedDashboardNumber leftFlywheelSetpoint = new LoggedDashboardNumber("Shooter/Flywheels/leftSetpoint", 6000);
+public class TuneShooterCommand extends ParallelCommandGroup {
+    private final LoggedDashboardNumber leftFlywheelSetpoint = new LoggedDashboardNumber("Shooter/Flywheels/leftSetpoint", 8000);
     private final LoggedDashboardNumber rightFlywheelSetpoint = new LoggedDashboardNumber("Shooter/Flywheels/rightSetpoint", 6000);
     private final LoggedDashboardNumber angleSetpoint = new LoggedDashboardNumber("Shooter/Pivot/angleSetpoint", 15);
-    private final Shooter shooter;
-    private final Indexer indexer;
 
+    private int shotNum = 0;
     public TuneShooterCommand(Shooter shooter, Indexer indexer) {
-        this.shooter = shooter;
-        this.indexer = indexer;
+
+        addCommands(
+                new IntakeAndFeed(indexer, ()->1, ()->1),
+                new PrepareShooterCommand(shooter, ()->
+                        new ShooterSetpoint(
+                                Rotation2d.fromDegrees(angleSetpoint.get()), leftFlywheelSetpoint.get(), rightFlywheelSetpoint.get()
+                        )),
+                Commands.sequence(
+                        Commands.waitUntil(indexer::isNoteStaged),
+                        Commands.waitUntil(()->!indexer.isNoteStaged()),
+                        Commands.waitSeconds(1),
+                        Commands.runOnce(()->shotNum += 1),
+                        Commands.print("Shot Number: " + shotNum + " Angle Degrees Setpoint: " + angleSetpoint.get() + " Left RPM Setpoint: " + leftFlywheelSetpoint.get() + " Right RPM Setpoint: " + rightFlywheelSetpoint.get())
+                ).repeatedly()
+        );
         addRequirements(shooter, indexer);
-    }
-
-    @Override
-    public void execute() {
-        shooter.setFlywheelSpeeds(leftFlywheelSetpoint.get(), rightFlywheelSetpoint.get());
-        shooter.setPivotPosition(Rotation2d.fromDegrees(angleSetpoint.get()));
-        indexer.setFeederPercent(1);
-        indexer.setIntakePercent(1);
-    }
-
-    @Override
-    public void end(boolean interrupted) {
-        shooter.stopFlywheels();
-        shooter.stopPivot();
-        indexer.stopAll();
     }
 }
