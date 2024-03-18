13d99e2a685435b9bf5636576fd35dbb78d23ae0
David :D <KongD@catlin.edu>
2024-03-18 15:47:08 -0700

    fix: Made elevator move to bottom before backing the note out :D

--- src/main/java/org/team1540/robot2024/commands/tramp/BackOutTramp.java
+++ src/main/java/org/team1540/robot2024/commands/tramp/BackOutTramp.java
@@ -3,15 +3,19 @@ package org.team1540.robot2024.commands.tramp;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
+import org.team1540.robot2024.Constants;
+import org.team1540.robot2024.commands.elevator.ElevatorSetpointCommand;
 import org.team1540.robot2024.commands.indexer.IntakeAndFeed;
 import org.team1540.robot2024.commands.indexer.IntakeCommand;
+import org.team1540.robot2024.subsystems.elevator.Elevator;
 import org.team1540.robot2024.subsystems.indexer.Indexer;
 import org.team1540.robot2024.subsystems.tramp.Tramp;
 
 public class BackOutTramp extends SequentialCommandGroup {
 
-    public BackOutTramp(Tramp tramp, Indexer indexer) {
+    public BackOutTramp(Tramp tramp, Indexer indexer, Elevator elevator) {
         addCommands(
+                new ElevatorSetpointCommand(elevator, Constants.Elevator.ElevatorState.BOTTOM),
                 Commands.deadline(
                         Commands.sequence(
                                 new WaitUntilCommand(indexer::isNoteStaged),
