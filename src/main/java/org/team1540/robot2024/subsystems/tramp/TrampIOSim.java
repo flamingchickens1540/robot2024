3fd8a41cb7fe7044070fa8ff8f462aa7314f7b60
Zach R <zarutman+git@gmail.com>
2024-03-20 23:10:40 -0700

    feat: add beam breaks to sim

--- src/main/java/org/team1540/robot2024/subsystems/tramp/TrampIOSim.java
+++ src/main/java/org/team1540/robot2024/subsystems/tramp/TrampIOSim.java
@@ -2,15 +2,18 @@ package org.team1540.robot2024.subsystems.tramp;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.simulation.DCMotorSim;
 import org.team1540.robot2024.Constants;
 
+import static org.team1540.robot2024.Constants.Tramp.BEAM_BREAK_CHANNEL;
 import static org.team1540.robot2024.Constants.Tramp.GEAR_RATIO;
 
 public class TrampIOSim implements TrampIO {
     private final DCMotorSim sim = new DCMotorSim(DCMotor.getNEO(1), GEAR_RATIO, 0.025); //TODO: Fix MOI its probably wrong :D
-
+    private final DigitalInput beamBreak = new DigitalInput(BEAM_BREAK_CHANNEL);
     private double appliedVolts = 0.0;
+
 
     @Override
     public void updateInputs(TrampIOInputs inputs) {
@@ -18,6 +21,7 @@ public class TrampIOSim implements TrampIO {
         inputs.velocityRPM = sim.getAngularVelocityRPM();
         inputs.appliedVolts = appliedVolts;
         inputs.currentAmps = sim.getCurrentDrawAmps();
+        inputs.noteInTramp =beamBreak.get();
     }
 
     @Override
