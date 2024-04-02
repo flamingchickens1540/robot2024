8090624894fa8769813fa2b063e3a1352b38eb5a
Alvin Zhang <41vin2h4n9@gmail.com>
2024-04-01 20:57:36 -0700

    fix: apply weird limelight offset correctly

--- src/main/java/org/team1540/robot2024/subsystems/vision/AprilTagVisionIOLimelight.java
+++ src/main/java/org/team1540/robot2024/subsystems/vision/AprilTagVisionIOLimelight.java
@@ -1,12 +1,6 @@
 package org.team1540.robot2024.subsystems.vision;
 
-import com.pathplanner.lib.util.GeometryUtil;
 import edu.wpi.first.math.geometry.Pose3d;
-import edu.wpi.first.math.geometry.Rotation3d;
-import edu.wpi.first.math.geometry.Transform3d;
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj.Timer;
 import org.team1540.robot2024.util.vision.LimelightHelpers;
 
 public class AprilTagVisionIOLimelight implements AprilTagVisionIO {
@@ -23,9 +17,12 @@ public class AprilTagVisionIOLimelight implements AprilTagVisionIO {
     public void updateInputs(AprilTagVisionIOInputs inputs) {
         LimelightHelpers.PoseEstimate measurement = LimelightHelpers.getBotPoseEstimate_wpiBlue(name);
         if (measurement.tagCount > 1) {
-            inputs.estimatedPoseMeters =
-                    LimelightHelpers.getBotPose3d_wpiBlue(name)
-                            .transformBy(new Transform3d(0, 0.105, 0, new Rotation3d()));
+            Pose3d limelightPose = LimelightHelpers.getBotPose3d_wpiBlue(name);
+            inputs.estimatedPoseMeters = new Pose3d(
+                    limelightPose.getX(),
+                    limelightPose.getY() - 10.5,
+                    limelightPose.getZ(),
+                    limelightPose.getRotation());
             inputs.lastMeasurementTimestampSecs = measurement.timestampSeconds;
         }
         inputs.numTagsSeen = measurement.tagCount;
