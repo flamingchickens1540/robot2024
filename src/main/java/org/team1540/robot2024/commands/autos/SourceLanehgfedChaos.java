cf11112d1ba6d9188a23f09a9f242d57d690301f
Simon Weil <113215817+WeilSimon@users.noreply.github.com>
2024-04-04 08:49:04 -0700

    feat: updated command

--- src/main/java/org/team1540/robot2024/commands/autos/SourceLanehgfedChaos.java
+++ src/main/java/org/team1540/robot2024/commands/autos/SourceLanehgfedChaos.java
@@ -1,18 +1,28 @@
 package org.team1540.robot2024.commands.autos;
 
+import edu.wpi.first.wpilibj2.command.Commands;
+import org.team1540.robot2024.Constants;
 import org.team1540.robot2024.commands.indexer.IntakeAndFeed;
+import org.team1540.robot2024.commands.shooter.PrepareShooterCommand;
+import org.team1540.robot2024.subsystems.drive.Drivetrain;
 import org.team1540.robot2024.subsystems.indexer.Indexer;
+import org.team1540.robot2024.subsystems.shooter.Shooter;
 import org.team1540.robot2024.util.auto.AutoCommand;
 import org.team1540.robot2024.util.auto.PathHelper;
+import org.team1540.robot2024.util.shooter.ShooterSetpoint;
 
 public class SourceLanehgfedChaos extends AutoCommand {
-    public  SourceLanehgfedChaos (Indexer indexer) {
+    public  SourceLanehgfedChaos (Drivetrain drivetrain, Shooter shooter, Indexer indexer) {
         super("SourceLanephgfedChaos");
         addPath(
                 PathHelper.fromChoreoPath("SourceLanehgfedChaos.1", true, true)
         );
-            addCommands(
-                    new IntakeAndFeed(indexer, () -> 1, () -> 0.05)//TODO: tune shoot speed to not eject note out of field
-            );
+        addCommands(
+                    Commands.parallel(
+                            getPath(0).getCommand(drivetrain),
+                            IntakeAndFeed.withDefaults(indexer),
+                            new PrepareShooterCommand(shooter, ()->new ShooterSetpoint(Constants.Shooter.Pivot.HUB_SHOOT.pivot, 1000, 1000))
+                    )
+        );
     }
 }
