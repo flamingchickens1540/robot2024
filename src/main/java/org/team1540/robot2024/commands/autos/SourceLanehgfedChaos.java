a0b9295f7f3a9c9d4e38741c8488f80ec161c582
belowAverageCoder5188 <146988904+belowAverageCoder5188@users.noreply.github.com>
2024-04-09 10:54:20 -0700

    Fix: Made the one true SourceLanehgfedChaos auto claim its throne

--- src/main/java/org/team1540/robot2024/commands/autos/SourceLanehgfedChaos.java
+++ src/main/java/org/team1540/robot2024/commands/autos/SourceLanehgfedChaos.java
@@ -1,18 +1,18 @@
 package org.team1540.robot2024.commands.autos;
 
+import org.team1540.robot2024.util.auto.AutoCommand;
 import org.team1540.robot2024.commands.indexer.IntakeAndFeed;
 import org.team1540.robot2024.subsystems.indexer.Indexer;
-import org.team1540.robot2024.util.auto.AutoCommand;
 import org.team1540.robot2024.util.auto.PathHelper;
 
 public class SourceLanehgfedChaos extends AutoCommand {
-    public  SourceLanehgfedChaos (Indexer indexer) {
-        super("SourceLanephgfedChaos");
+    public SourceLanehgfedChaos(Indexer indexer) {
+        super("SourceLanephgfedChaos2");
         addPath(
                 PathHelper.fromChoreoPath("SourceLanehgfedChaos.1", true, true)
         );
-            addCommands(
-                    new IntakeAndFeed(indexer, () -> 1, () -> 0.05)//TODO: tune shoot speed to not eject note out of field
-            );
+        addCommands(
+                new IntakeAndFeed(indexer, () -> 1, () -> 0.05)//TODO: tune shoot speed to not eject note out of field
+        );
     }
 }
