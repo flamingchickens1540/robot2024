bbd8809855c9f04b2956a60e5c9b9d9bc56531e2
Luna <gonzalezgonzalezl@catlin.edu>
2024-05-03 18:17:15 -0700

    feat: kidMode :)

---
+++ src/main/java/org/team1540/robot2024/commands/drivetrain/KidModeSwerveDriveCommand.java
@@ -0,0 +1,39 @@
+package org.team1540.robot2024.commands.drivetrain;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import org.team1540.robot2024.Constants;
+import org.team1540.robot2024.subsystems.drive.Drivetrain;
+import org.team1540.robot2024.util.LoggedTunableNumber;
+import org.team1540.robot2024.util.math.JoystickUtils;
+
+public class KidModeSwerveDriveCommand extends Command {
+    private final Drivetrain drivetrain;
+    private final CommandXboxController controller;
+    private double maxSpeedPercentage;
+    private double maxRotsPercentage;
+    LoggedTunableNumber kidModeSpeedPercent = new LoggedTunableNumber("kidMode/maxSpeedPercentage", 0.3);
+    LoggedTunableNumber kidModeRotsPercent = new LoggedTunableNumber("kidMode/maxRotsPercentage", 0.5);
+
+
+    public KidModeSwerveDriveCommand(Drivetrain drivetrain, CommandXboxController controller){
+        this.drivetrain = drivetrain;
+        this.controller = controller;
+    }
+
+    public void execute() {
+        //TODO: check deadbands bc of diff controllers
+        double xPercent = MathUtil.applyDeadband((-controller.getLeftY()), 0);
+        double yPercent = MathUtil.applyDeadband((-controller.getLeftX()), 0);
+        double rotPercent = MathUtil.applyDeadband((-controller.getRightX()), 0.1);
+
+        double linearMagnitude = JoystickUtils.smartDeadzone(Math.hypot(xPercent, yPercent), 0.2);
+        Rotation2d linearDirection = new Rotation2d(xPercent, yPercent);
+
+        drivetrain.drivePercent(linearMagnitude * kidModeSpeedPercent.get(), linearDirection, rotPercent * kidModeRotsPercent.get(), true);
+    }
+
+
+}
