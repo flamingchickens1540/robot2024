f64ab33b75fadd3f408ddadaec603a5346f2cbe1
Alvin Zhang <41vin2h4n9@gmail.com>
2024-05-21 09:17:07 -0700

    fix: stop showing gyro disconnect alert in sim

--- src/main/java/org/team1540/robot2024/subsystems/drive/Drivetrain.java
+++ src/main/java/org/team1540/robot2024/subsystems/drive/Drivetrain.java
@@ -27,6 +27,7 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import org.littletonrobotics.junction.AutoLogOutput;
 import org.littletonrobotics.junction.Logger;
+import org.team1540.robot2024.Robot;
 import org.team1540.robot2024.util.Alert;
 import org.team1540.robot2024.util.auto.LocalADStarAK;
 import org.team1540.robot2024.Constants;
@@ -195,7 +196,7 @@ public class Drivetrain extends SubsystemBase {
         poseEstimator.update(rawGyroRotation, getModulePositions());
         visionPoseEstimator.update(rawGyroRotation, getModulePositions());
 
-        gyroDisconnected.set(!gyroInputs.connected);
+        gyroDisconnected.set(!gyroInputs.connected && Robot.isReal());
     }
 
     /**
